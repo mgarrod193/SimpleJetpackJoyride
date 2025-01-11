255beda9578ad68898f6d03697c152f362e26885
mgarrod <mgarrod193@gmail.com>
2025-01-11 17:13:00 +0000

    Barrel obstacle added
    created a barrel obstacle that rotates and moves towards the left of screen, if barrel collides with player the player is destroyed

---
+++ Assets/Scripts/Obstacle/Barrel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    private Rigidbody2D objRb;
+    [SerializeField] float rotationSpeed = 50;
+    [SerializeField] Vector2 speed = new Vector2(-10f, 0);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objRb.velocity = speed;
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player" )
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
